fb733263b826b33a46c0a553c3e0ebe44a24706c
brunoquagliottii <brunoquagliotti@outlook.com>
2023-04-04 21:55:00 -0300

    Implementando setup e teardown em teste de integração

--- testes.py
+++ testes.py
@@ -1,23 +1,36 @@
 import unittest
 import requests
 
+
 class TestAPI(unittest.TestCase):
 
+    def setUp(self):
+        self.base_url = 'http://localhost:5000'
+
+        requests.get(f'{self.base_url}/apagar')
+
+        requests.get(f'{self.base_url}/criar')
+
+    def tearDown(self):
+        requests.get(f'{self.base_url}/apagar')
+        pass
+
     def test_criar_banco_url(self):
-        response = requests.get('http://localhost:5000/criar')
+        response = requests.get(f'{self.base_url}/criar')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, 'criado')
 
     def test_apagar_banco_url(self):
-        response = requests.get('http://localhost:5000/apagar')
+        response = requests.get(f'{self.base_url}/apagar')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, 'apagado')
 
     def test_buscar_nome_filme(self):
-        response = requests.get('http://localhost:5000/teste?name=taxi%20driver')
+        response = requests.get(f'{self.base_url}/teste?name=taxi%20driver')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()), 1)
         self.assertEqual(response.json()[0]['nome'], 'Taxi Driver')
+
 
 if __name__ == '__main__':
     unittest.main()
